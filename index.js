84f4947f040107d5d57790dc2325637c58d7aa07
abhishek <abhishek7gg7@gmail.com>
2020-02-29 13:55:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,64 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+app.use(express.json());
 
-app.get("/*", function(req,res) {
-
+app.post("/*", async function(req,res) {
+var msg = req.body.queryResult.queryText;
+res.json({
+  "fulfillmentText": msg,
+  "fulfillmentMessages": [
+    {
+      "card": {
+        "title": "card title",
+        "subtitle": "card text",
+        "imageUri": "https://example.com/images/example.png",
+        "buttons": [
+          {
+            "text": "button text",
+            "postback": "https://example.com/path/for/end-user/to/follow"
+          }
+        ]
+      }
+    }
+  ],
+  "source": "example.com",
+  "payload": {
+    "google": {
+      "expectUserResponse": true,
+      "richResponse": {
+        "items": [
+          {
+            "simpleResponse": {
+              "textToSpeech": "this is a simple response"
+            }
+          }
+        ]
+      }
+    },
+    "facebook": {
+      "text": msg
+    },
+    "slack": {
+      "text": msg
+    }
+  },
+  "outputContexts": [
+    {
+      "name": "projects/project-id/agent/sessions/session-id/contexts/context-name",
+      "lifespanCount": 5,
+      "parameters": {
+        "param-name": "param-value"
+      }
+    }
+  ],
+  "followupEventInput": {
+    "name": "event name",
+    "languageCode": "en-US",
+    "parameters": {
+      "param-name": "param-value"
+    }
+  }
+})
 })
 
 app.listen(process.env.PORT);
