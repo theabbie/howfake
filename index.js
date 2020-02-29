2133dc27c9630d7a57bdf064f8e00e20565b824d
abhishek <abhishek7gg7@gmail.com>
2020-02-29 17:27:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
 var express = require('express');
 var app = express();
 app.use(express.json());
+var axios = require("axios");
 
 app.post("/*", async function(req,res) {
 var msg = req.body.queryResult.queryText;
 res.json({
   "fulfillmentText": msg,
-  "fulfillmentMessages": [
-    {
-      "card": {
-        "title": "card title",
-        "subtitle": "card text",
-        "imageUri": "https://example.com/images/example.png",
-        "buttons": [
-          {
-            "text": "button text",
-            "postback": "https://example.com/path/for/end-user/to/follow"
-          }
-        ]
-      }
-    }
-  ],
-  "source": "example.com",
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "this is a simple response"
-            }
-          }
-        ]
-      }
-    },
-    "facebook": {
-      "text": msg
-    },
-    "slack": {
-      "text": msg
-    }
-  },
-  "outputContexts": [
+    "outputContexts": [
     {
       "name": "projects/project-id/agent/sessions/session-id/contexts/context-name",
       "lifespanCount": 5,
